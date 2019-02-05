84feefe07064a0bb589ccada6b3825fb62e89a56
Atomization <stevecoulter99@gmail.com>
2019-02-05 17:31:27 -0600

    No need for listener in main

--- src/com/stephen/coolbros/Main.java
+++ src/com/stephen/coolbros/Main.java
@@ -6,10 +6,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 
     @Override
     public void onEnable(){
