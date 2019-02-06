8f83ad8845428f0c2f15025368f977469e17e8f3
Atomization <stevecoulter99@gmail.com>
2019-02-05 18:14:05 -0600

    Update JoinEvent.java

--- src/com/stephen/coolbros/events/JoinEvent.java
+++ src/com/stephen/coolbros/events/JoinEvent.java
@@ -1,5 +1,6 @@
 package com.stephen.coolbros.events;
 
+import com.stephen.coolbros.Colorize;
 import com.stephen.coolbros.Main;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,10 +10,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinEvent implements Listener {
 
     private Main plugin;
+    private Colorize colorize;
 
     /** Constructor */
     public JoinEvent(Main plugin){
         this.plugin = plugin;
+        this.colorize = new Colorize(plugin);
     }
 
     @EventHandler
@@ -20,7 +23,7 @@ public class JoinEvent implements Listener {
         Player player = event.getPlayer();
         String message = this.plugin.getConfig().getString(player.getName());
         if (this.plugin.getConfig().isSet(player.getName())) {
-            event.setJoinMessage(this.plugin.addColor().colorize(message).replace("{player}", player.getName()));
+            event.setJoinMessage(this.colorize.addColor(message).replace("{player}", player.getName()));
         }
     }
 
