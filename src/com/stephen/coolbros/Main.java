0b59101e75de196e0d7e8fb59a808e7715bfb7e0
Atomization <stevecoulter99@gmail.com>
2019-02-05 18:12:40 -0600

    Optimize code

--- src/com/stephen/coolbros/Main.java
+++ src/com/stephen/coolbros/Main.java
@@ -1,39 +1,19 @@
 package com.stephen.coolbros;
 
+import com.stephen.coolbros.commands.SetJoinMessageCommand;
 import com.stephen.coolbros.events.JoinEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable(){
+        getCommand("setjoinmessage").setExecutor(new SetJoinMessageCommand(this));
+        Bukkit.getPluginManager().registerEvents(new JoinEvent(this), this);
+
         this.getConfig().options().copyDefaults();
         saveDefaultConfig();
-
-        Bukkit.getPluginManager().registerEvents(new JoinEvent(this), this);
     }
-
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equals("setjoinmessage")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                String message = String.join(" ", args);
-                this.getConfig().set(player.getName(), message);
-                this.saveConfig();
-                player.sendMessage( ChatColor.BOLD +""+ ChatColor.GOLD + "Set your join message to:");
-                player.sendMessage(ChatColor.BOLD +"» "+ ChatColor.RESET + addColor().colorize(message).replace("{player}", player.getName()));
-            }
-        }
-
-        return false;
-    }
-
-    public ColorText addColor(){
-        return new ColorText(this);
-    }
+    
 }
